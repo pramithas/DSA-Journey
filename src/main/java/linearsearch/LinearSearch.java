7b499c3435c70f40153f5b75f62cde3ff120fd9f
Pramithas Dhakal <pramithas.dhakal@renegadeinsurance.com>
2023-05-10 07:22:39 +0545

    Initial commit after adding gitignore file

---
+++ src/main/java/linearsearch/LinearSearch.java
@@ -0,0 +1,22 @@
+package linearsearch;
+
+public class LinearSearch {
+
+    public static int linearSearch(int [] arr, int searchTerm){
+        if(arr.length == 0){
+            return -1;
+        }
+
+        for(int i=0;i< arr.length;i++){
+            if(arr[i] == searchTerm){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,5,9,8,4};
+        System.out.println(linearSearch(arr, 9));
+    }
+}
