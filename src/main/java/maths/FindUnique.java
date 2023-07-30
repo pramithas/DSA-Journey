5691c93648e9d009093c800193f75757d4cb48b7
pramithas <dhakalpramithas@gmail.com>
2023-07-30 15:28:41 +0545

    Add program to find unique number

---
+++ src/main/java/maths/FindUnique.java
@@ -0,0 +1,23 @@
+package maths;
+
+/**
+ * Bitwise XOR operation.
+ * Find unique in a an array where every other number has a duplicate.
+ */
+public class FindUnique {
+
+    public static int findUnique(int[] arr){
+        int unique = 0;
+        for(int elem: arr){
+            unique ^= elem;
+        }
+        return unique;
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {2,3,3,4,2,6,4,6,7};
+        int uniqueNumber = findUnique(arr);
+        System.out.println(uniqueNumber);
+    }
+}
