42fe1c9c0d5e386bbad510845e472ad7ab84405f
pramithas <dhakalpramithas@gmail.com>
2023-07-31 21:10:24 +0545

    Write a program to find nth magic number

--- src/main/java/maths/MagicNumber.java
+++ src/main/java/maths/MagicNumber.java
@@ -1,2 +1,22 @@
-package maths;public class MagicNumber {
+package maths;
+
+/**
+ * Write a program to find nth magic number.
+ */
+public class MagicNumber {
+
+    public static int findMagicNumber(int n){
+        int magicNumber = 0;
+        int counter = 1;
+        while(n > 0){
+            int lastDigit = n & 1;
+            magicNumber += lastDigit * (Math.pow(5, counter++));
+            n = n >> 1;
+        }
+        return magicNumber;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMagicNumber(5));
+    }
 }
