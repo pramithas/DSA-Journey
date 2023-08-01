414b33ebe8b295dd176a1fcd6910bb81c47f06d7
pramithas <dhakalpramithas@gmail.com>
2023-08-01 21:39:23 +0545

    Write a program to check if a number of power of two

---
+++ src/main/java/maths/PowerOfTwo.java
@@ -0,0 +1,14 @@
+package maths;
+
+/**
+ * Write a program that checks if a number if power of two
+ */
+public class PowerOfTwo {
+    public static boolean isPowerOfTwo(int n){
+        return (n & (n-1)) == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(33));
+    }
+}
