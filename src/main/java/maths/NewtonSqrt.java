2c6cb5df26a13b78c2eae3bbf1025ca0f283ab6d
pramithas <dhakalpramithas@gmail.com>
2023-08-02 17:01:40 +0545

    Square root newton raphson method

---
+++ src/main/java/maths/NewtonSqrt.java
@@ -0,0 +1,22 @@
+package maths;
+
+public class NewtonSqrt {
+
+    static double sqrt(double n){
+        double x = n;
+        double root;
+        while (true){
+            root = 0.5 * (x+ (n/x));
+
+            if(Math.abs(root -x) < 0.000001)
+                break;
+
+            x = root;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sqrt(40));
+    }
+}
