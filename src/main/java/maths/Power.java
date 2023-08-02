8ca7ab1021dbd1d6bafbab1d89d0ca56a8e2ff52
pramithas <dhakalpramithas@gmail.com>
2023-08-02 12:44:18 +0545

    Write a program to find a to the power b

---
+++ src/main/java/maths/Power.java
@@ -0,0 +1,26 @@
+package maths;
+
+/**
+ * Write a program to power a number to a given number
+ */
+public class Power {
+
+    public static int findPower(int base, int power){
+
+        int ans = 1;
+
+        while(power > 0){
+            if((power & 1) == 1){
+                ans *= base;
+            }
+
+            base *= base;
+            power = power >> 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findPower(2,5));
+    }
+}
