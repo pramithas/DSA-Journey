557bb14c8bed2da52185a4b052137498402fcfb9
pramithas <dhakalpramithas@gmail.com>
2023-08-23 11:17:57 +0545

    Add code to generate subsets using iteration

--- src/main/java/recursion/subsequence/SubsetIteration.java
+++ src/main/java/recursion/subsequence/SubsetIteration.java
@@ -1,4 +1,30 @@
 package recursion.subsequence;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SubsetIteration {
+
+    public static void main(String[] args) {
+        int str[] = {1, 2, 3};
+        findSubsets(str);
+    }
+
+    public static void findSubsets(int arr[]) {
+
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for (int str : arr) {
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> inner = new ArrayList<>(outer.get(i));
+                inner.add(str);
+                outer.add(inner);
+            }
+        }
+
+        System.out.println(Arrays.toString(outer.toArray()));
+    }
 }
