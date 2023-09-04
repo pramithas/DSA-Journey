2d3ecbacd0e88648a7ce95cfc057f7d0dd68487f
pramithas <dhakalpramithas@gmail.com>
2023-09-04 10:53:30 +0545

    Subset with dupicates in the input

---
+++ src/main/java/recursion/subsequence/SubsequenceWithDuplicate.java
@@ -0,0 +1,44 @@
+package recursion.subsequence;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubsequenceWithDuplicate {
+
+    public static void main(String[] args) {
+        int arr[] = {5, 5, 5, 5, 5};
+        System.out.println(Arrays.toString(subsetWithoutDuplicates(arr).toArray()));
+    }
+
+    public static List<List<Integer>> subsetWithoutDuplicates(int[] nums) {
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        int start;
+        int end = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            start = 0;
+            int n = outer.size();
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                start = end + 1;
+            }
+
+            end = outer.size() - 1;
+
+            for (int j = start; j < n; j++) {
+                List<Integer> innerList = new ArrayList<>(outer.get(j));
+                innerList.add(nums[i]);
+                outer.add(innerList);
+            }
+        }
+
+        return outer;
+    }
+}
