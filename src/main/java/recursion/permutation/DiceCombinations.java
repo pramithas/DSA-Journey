69ee7f74cbbbed4d14a71286ba951ac0e0d6cac5
pramithas <dhakalpramithas@gmail.com>
2023-09-05 10:03:54 +0545

    Rolling dice problem

---
+++ src/main/java/recursion/permutation/DiceCombinations.java
@@ -0,0 +1,28 @@
+package recursion.permutation;
+
+public class DiceCombinations {
+
+    public static void main(String[] args) {
+        System.out.println(dice(4, 6));
+    }
+
+    public static int dice(int target, int k) {
+        int count = 0;
+        return solve(target, k, "", count);
+    }
+
+    static int solve(int target, int k, String processed, int count) {
+
+        int finalCount = 0;
+
+        if (target == 0) {
+            count++;
+            return count;
+        }
+
+        for (int i = 1; i <= k && i <= target; i++) {
+            finalCount += solve(target - i, k, (processed + i), count);
+        }
+        return finalCount;
+    }
+}
--- src/main/java/recursion/permutation/LetterCombinationPhone.java
+++ src/main/java/recursion/permutation/LetterCombinationPhone.java
@@ -1,24 +1,56 @@
 package recursion.permutation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LetterCombinationPhone {
     public static void main(String[] args) {
-        pad("", "12");
+        System.out.println(Arrays.toString(findPermuations("9").toArray()));
     }
 
-    private static void pad(String p, String up) {
+    public static List<String> findPermuations(String number) {
+        List<String> result = new ArrayList<>();
+
+        if (number.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        pad("", number, result);
+        return result;
+    }
+
+
+    private static void pad(String p, String up, List<String> result) {
 
         if (up.isEmpty()) {
-            System.out.println(p);
+            result.add(p);
             return;
         }
 
         // This is just used to calculate the range.
         int digit = up.charAt(0) - '0';
 
+        int start = (digit - 2) * 3;
+        int end = (digit - 1) * 3;
 
-        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+        if (digit == 7) {
+            end += 1;
+        }
+
+        if (digit == 8) {
+            start += 1;
+            end+=1;
+        }
+
+        if (digit == 9) {
+            start += 1;
+            end+=2;
+        }
+
+        for (int i = start; i < end; i++) {
             char ch = (char) ('a' + i);
-            pad(p + ch, up.substring(1));
+            pad(p + ch, up.substring(1), result);
         }
     }
 }
