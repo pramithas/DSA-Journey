643f7d50050e97a363ba644ce4c594af60c1816c
pramithas <dhakalpramithas@gmail.com>
2023-09-24 11:10:46 +0545

    Implement circular queue

---
+++ src/main/java/stacks_queues/DynamicQueue.java
@@ -0,0 +1,28 @@
+package stacks_queues;
+
+public class DynamicQueue extends CircularQueue {
+
+    public DynamicQueue() {
+        super();
+    }
+
+    public DynamicQueue(int size) {
+        super(size);
+    }
+
+    public boolean insert(int item) {
+        if (this.isFull()) {
+            // double the array size
+            int[] temp = new int[data.length * 2];
+
+            // copy all previous items in new data
+            for (int i = 0; i < data.length; i++) {
+                temp[i] = data[(front + i) % data.length];
+            }
+
+            front = 0;
+            end = data.length
+            data = temp;
+        }
+    }
+}
