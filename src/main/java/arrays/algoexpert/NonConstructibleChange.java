0aee5ec44572970a208cdb7c1a852402ca26666e
pramithas <dhakalpramithas@gmail.com>
2023-10-18 09:37:29 +0545

    Solve non constructible change

--- src/main/java/arrays/algoexpert/NonConstructibleChange.java
+++ src/main/java/arrays/algoexpert/NonConstructibleChange.java
@@ -1,7 +1,33 @@
 package arrays.algoexpert;
 
+import java.util.Arrays;
+
 /**
  * https://www.algoexpert.io/questions/non-constructible-change
  */
 public class NonConstructibleChange {
+
+    // Write your code here.
+    public int nonConstructibleChange(int[] coins) {
+        // Write your code here.
+
+        Arrays.sort(coins);
+
+        int sum = 0;
+
+        for (int i = 0; i < coins.length; i++) {
+
+            if (coins[i] > (sum + 1)) {
+                return sum + 1;
+            }
+            sum += coins[i];
+        }
+        return sum + 1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 7, 1, 1, 2, 3, 22};
+        int result = new NonConstructibleChange().nonConstructibleChange(arr);
+        System.out.println(result);
+    }
 }
