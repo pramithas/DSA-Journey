bb1b031b91daff366e537943099cccfcf0cb2b21
Pramithas Dhakal <dhakalpramithas@gmail.com>
2023-12-21 14:55:59 +0545

    Add second revision notes on Dec 21, 2023

--- src/main/java/recursion/backtracking/MazeAllDirectionsWithPath.java
+++ src/main/java/recursion/backtracking/MazeAllDirectionsWithPath.java
@@ -2,6 +2,10 @@ package recursion.backtracking;
 
 import java.util.Arrays;
 
+/**
+ * Revised on Dec, 21.
+ * This is introduction to backtracking.
+ */
 public class MazeAllDirectionsWithPath {
 
     public static void main(String[] args) {
@@ -13,7 +17,13 @@ public class MazeAllDirectionsWithPath {
     static void mazeAllDirectionsWithPath(int r, int c, boolean maze[][], int[][] path, int step, String processed) {
 
         if (r == maze.length - 1 && c == maze[0].length - 1) {
+
             path[r][c] = step;
+
+            // Just printing the path.
+            // [0, 0, 0]
+            // [0, 1, 2]
+            // [0, 0, 3]
             for (int[] arr : path) {
                 System.out.println(Arrays.toString(arr));
             }
@@ -23,11 +33,14 @@ public class MazeAllDirectionsWithPath {
             return;
         }
 
+        // Step 2: Do not visit the already visited node.
         if (!maze[r][c]) {
             return;
         }
 
+        // Step: 1 Mark the visited node as false, so that it is not visited again.
         maze[r][c] = false;
+        // Just tracking that path.
         path[r][c] = step;
 
         // Not just center diagonal. It means diagonal movement from any cell to other cell.
@@ -53,6 +66,7 @@ public class MazeAllDirectionsWithPath {
             mazeAllDirectionsWithPath(r, c - 1, maze, path, step + 1, processed + "L");
         }
 
+        // The value should be reset, before the recursion call returns.
         maze[r][c] = true;
         path[r][c] = 0;
     }
