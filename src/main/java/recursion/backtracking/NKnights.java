c39bc3eec67d612fc177a4d6c62d3c7244453290
Pramithas Dhakal <dhakalpramithas@gmail.com>
2023-12-21 18:04:37 +0545

    Revise, add notes in backtracking codes

--- src/main/java/recursion/backtracking/NKnights.java
+++ src/main/java/recursion/backtracking/NKnights.java
@@ -1,5 +1,8 @@
 package recursion.backtracking;
 
+/**
+ * There is a n*n board and we need to place x queens in a way they don't attack each other.
+ */
 public class NKnights {
 
     public static void main(String[] args) {
@@ -11,39 +14,48 @@ public class NKnights {
         solveNKnight(board, 0, 0, 4);
     }
 
+    /*
+       Knights tracks how much knights are remaining.
+     */
     private static void solveNKnight(boolean[][] board, int row, int col, int knights) {
 
+        // all the knights have been placed, so just display the board.
         if (knights == 0) {
             display(board);
             System.out.println();
             return;
         }
 
+        // Why?
         if (row == board.length - 1 && col == board.length) {
             return;
         }
 
-//        if (col == board.length) {
-//            solveNKnight(board, row + 1, 0, knights);
-//            // Have never seen this pattern. i.e. return after recursive method call.
-//            return;
-//        }
-
         if (col == board.length) {
-            row += 1;
-            col = 0;
+            solveNKnight(board, row + 1, 0, knights);
+            // Have never seen this pattern. i.e. return after recursive method call.
+            return;
         }
+
+//        if (col == board.length) {
+//            row += 1;
+//            col = 0;
+//        }
 
         if (isSafe(board, row, col)) {
             board[row][col] = true;
+            // is a correct spot is found, try the next column.
             solveNKnight(board, row, col + 1, knights - 1);
             board[row][col] = false;
         }
+
+        // If the current position is not safe, move on to the next column.
 
         solveNKnight(board, row, col + 1, knights);
     }
 
     private static boolean isSafe(boolean[][] board, int row, int col) {
+
         // Only check the position upper to the current row.
         if (isValid(row - 1, col + 2, board.length)) {
             if (board[row - 1][col + 2]) {
--- src/main/java/recursion/backtracking/NQueensProblem.java
+++ src/main/java/recursion/backtracking/NQueensProblem.java
@@ -1,27 +1,25 @@
 package recursion.backtracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class NQueensProblem {
 
     public static void main(String[] args) {
-        solveNQueens(1);
+        displayResult(solveNQueens(6));
     }
 
     public static List<List<String>> solveNQueens(int n) {
-        String[][] board = new String[n][n];
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = ".";
-            }
-        }
-
+        String[][] board = createBoard(n);
         List<List<String>> lists = new ArrayList<>();
+
         queens(board, 0, lists);
         return lists;
     }
+
+
 
     public static int queens(String[][] board, int row, List<List<String>> lists) {
 
@@ -35,17 +33,7 @@ public class NQueensProblem {
 
         // All the queen positions have been found.
         if (row == board.length) {
-            List<String> innerList = new ArrayList<>();
-            for (String[] str : board) {
-                StringBuilder inner = new StringBuilder();
-                for (String elem : str) {
-                    inner.append(elem);
-                }
-                innerList.add(inner.toString());
-            }
-
-            lists.add(innerList);
-
+            lists.add(prepareResult(board));
             return 1;
         }
 
@@ -53,16 +41,34 @@ public class NQueensProblem {
 
         //placing the queen and checking for every row and col.
         for (int col = 0; col < board.length; col++) {
+            // If safe will always be true for first row.
             if (isSafe(board, row, col)) {
                 board[row][col] = "Q";
                 count += queens(board, row + 1, lists);
+                // Backtracking.
                 board[row][col] = ".";
             }
         }
         return count;
     }
 
+    private static List<String> prepareResult(String[][] board) {
+        List<String> innerList = new ArrayList<>();
+        for (String[] str : board) {
+            StringBuilder inner = new StringBuilder();
+            for (String elem : str) {
+                inner.append(elem);
+            }
+            // One list for per row.
+            innerList.add(inner.toString());
+        }
+
+        return innerList;
+    }
+
     private static boolean isSafe(String[][] board, int row, int col) {
+
+        // Need to only check up,left and right. Because, there are no queen below, yet.
 
         // check up
         for (int i = 0; i < row; i++) {
@@ -71,7 +77,10 @@ public class NQueensProblem {
             }
         }
 
+        // In both diagonal left and right, the row will decrease, since we are going up. That's why, (row - i)
+
         //diagonal left
+        // The columns to the left is just, col. So, min(row,col)
         int maxLeft = Math.min(row, col);
         for (int i = 1; i <= maxLeft; i++) {
             if (board[row - i][col - i].equalsIgnoreCase("Q")) {
@@ -80,6 +89,7 @@ public class NQueensProblem {
         }
 
         //diagonal right
+        // The columns to the right is board.length - col - 1.
         int maxRight = Math.min(row, board.length - col - 1);
         for (int i = 1; i <= maxRight; i++) {
             if (board[row - i][col + i].equalsIgnoreCase("Q")) {
@@ -102,6 +112,29 @@ public class NQueensProblem {
             }
             System.out.println();
         }
+    }
+
+    private static String[][] createBoard(int n) {
+
+        String[][] board = new String[n][n];
+
+        for (String[] strings : board) {
+            Arrays.fill(strings, ".");
+        }
+
+        return board;
+    }
+
+    private static void displayResult(List<List<String>> result) {
+
+        for(List<String> stringList: result){
+            for (String elem: stringList){
+                System.out.println(elem);
+            }
+            System.out.println();
+        }
+
+        System.out.println(result.size());
     }
 
 }
