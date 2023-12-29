b8071cfb61933105e859febe936dc3eb653a47b0
Pramithas Dhakal <dhakalpramithas@gmail.com>
2023-12-29 15:48:53 +0545

    Went through lowest common ancestor code and revised it

--- src/main/java/trees/problems/DFS/LowestCommonAncestor.java
+++ src/main/java/trees/problems/DFS/LowestCommonAncestor.java
@@ -2,26 +2,35 @@ package trees.problems.DFS;
 
 import trees.TreeNode;
 
+/**
+ * Revised on December, 29.
+ */
 public class LowestCommonAncestor {
 
+    /*
+      We have to find the answer as soon as possible closer to the root.
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
-        if(root == null){
+        if (root == null) {
             return null;
         }
 
-        if(root.val == p.val || root.val == q.val){
+        // Don't we need to check whether the other node exists or not?
+        if (root.val == p.val || root.val == q.val) {
             return root;
         }
 
+        // So, the left will be either null or the matching node.
         TreeNode left = lowestCommonAncestor(root.left, p, q);
+        // The right will be either null or matching node.
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        if(left != null && right != null){
+        if (left != null && right != null) {
             return root;
-        }else if(left == null){
+        } else if (left == null) {
             return right;
-        }else{
+        } else {
             return left;
         }
     }
