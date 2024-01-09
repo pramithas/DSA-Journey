aef06df2d78ba2330b892437b4e3aff80854d868
Pramithas Dhakal <dhakalpramithas@gmail.com>
2024-01-09 11:54:08 +0545

    Use removeFirst() method of java 21 to remove the first element of list

--- src/main/java/trees/problems/DFS/SerializeDeserializeBT.java
+++ src/main/java/trees/problems/DFS/SerializeDeserializeBT.java
@@ -8,20 +8,36 @@ import java.util.*;
 
 /**
  * Revised on 8 Jan.
+ * <p>
+ * If we capture the null nodes as well, then we do not need multiple traversals to construct a tree.
+ * One traversal is enough.
  */
 public class SerializeDeserializeBT {
 
     private String data;
 
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.populate(new Scanner(System.in));
+        binaryTree.display();
+        SerializeDeserializeBT ser = new SerializeDeserializeBT();
+        SerializeDeserializeBT deser = new SerializeDeserializeBT();
+        String serialized = ser.serialize(binaryTree.getRoot());
+        //String serialized = "1,2,null,null,3,4,null,null,5,null,null";
+        //  String serialized = "1,2,null,null,3,4,null,null,5,null,null";
+        TreeNode ans = deser.deserialize(serialized);
+        System.out.println();
+    }
+
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
 
         String s = serialize(root, "");
-        s = s.replaceAll("[,]+",",");
-        if(s.startsWith(",")){
+        s = s.replaceAll("[,]+", ",");
+        if (s.startsWith(",")) {
             s = s.substring(1);
         }
-        if(s.endsWith(",")){
+        if (s.endsWith(",")) {
             s = s.substring(0, s.length() - 1);
         }
         return s;
@@ -48,40 +64,29 @@ public class SerializeDeserializeBT {
         }
         String[] strArray = data.split(",");
         List<String> list = new ArrayList<String>(Arrays.asList(strArray));
-        Collections.reverse(list);
+        //Collections.reverse(list);
         return helper(list);
     }
 
     private TreeNode helper(List<String> list) {
 
-        if (list == null || list.size() == 0) {
+        if (list == null || list.isEmpty()) {
             return null;
         }
-        String val = list.remove(list.size() - 1);
+        String val = list.removeFirst();
 
         if (val.charAt(0) == 'n') {
             return null;
         }
 
+        // We are creating the nodes and going on left. When we reach the last node, it's left
+        // will be set to null and right to null as well. And, the node will be returned to the
+        // calling function.
         TreeNode node = new TreeNode(Integer.parseInt(val));
 
         node.left = helper(list);
         node.right = helper(list);
 
         return node;
-    }
-
-
-    public static void main(String[] args) {
-        BinaryTree binaryTree = new BinaryTree();
-        binaryTree.populate(new Scanner(System.in));
-        binaryTree.display();
-        SerializeDeserializeBT ser = new SerializeDeserializeBT();
-        SerializeDeserializeBT deser = new SerializeDeserializeBT();
-        String serialized = ser.serialize(binaryTree.getRoot());
-        //String serialized = "1,2,null,null,3,4,null,null,5,null,null";
-        //  String serialized = "1,2,null,null,3,4,null,null,5,null,null";
-        TreeNode ans = deser.deserialize(serialized);
-        System.out.println("");
     }
 }
