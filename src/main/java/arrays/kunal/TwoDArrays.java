c6c1ab1a8ec035b624d84e0c03b228eb164cf3f6
Pramithas Dhakal <dhakalpramithas@gmail.com>
2024-01-19 21:21:33 +0545

    Add example of 2D array

---
+++ src/main/java/arrays/kunal/TwoDArrays.java
@@ -0,0 +1,16 @@
+package arrays.kunal;
+
+public class TwoDArrays {
+
+    public static void main(String[] args) {
+
+        /*
+            1 2 3
+            4 5 6
+            7 8 9
+         */
+
+        // Column size is not required.
+        int[][] arr = new int[3][];
+    }
+}
