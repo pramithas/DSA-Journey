f08f176bf87124b750c1f6c6e3be953d555bf959
Pramithas Dhakal <dhakalpramithas@gmail.com>
2024-07-19 18:08:55 +0545

    Add solution for sunset views

--- src/main/java/stacks_queues/problems/SunsetViews.java
+++ src/main/java/stacks_queues/problems/SunsetViews.java
@@ -1,4 +1,32 @@
 package stacks_queues.problems;
 
+import java.util.ArrayList;
+
 public class SunsetViews {
+    public ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if(direction.equals("EAST")){
+
+            int max = Integer.MIN_VALUE;
+
+            for(int i = buildings.length - 1 ; i >=0 ; i--){
+                if(buildings[i] > max){
+                    result.addFirst(i);
+                    max = buildings[i];
+                }
+            }
+        }else {
+            int max = Integer.MIN_VALUE;
+
+            for(int i = 0; i <= buildings.length - 1 ; i++){
+                if(buildings[i] > max){
+                    result.add(i);
+                    max = buildings[i];
+                }
+            }
+        }
+        return result;
+    }
 }
